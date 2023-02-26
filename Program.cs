3510de202efb11f7b03be70a07ba0da2d5b4c193
Maks <maks-lug333@yandex.ru>
2023-02-26 21:54:55 +0900

    Ok

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} в степени {B} = {PowMetd(B)}");
+
+int PowMetd(int B)
+{
+    int power = 1;
+    for (int i = 0; i < B; i++)
+    {
+        power *= A;
+    }
+    return power;
+}
